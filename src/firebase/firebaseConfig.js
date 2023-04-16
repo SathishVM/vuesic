5b13ad1278594cf72076c5baa95dc2659b288c06
Sathish <sathishvm37@gmail.com>
2023-04-16 22:12:56 +0530

    use single quote instead of double

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: "AIzaSyBUoyQr4q_eXdpADcVrW7eCZRfppbZfNEg",
-  authDomain: "vuesic-7861f.firebaseapp.com",
-  projectId: "vuesic-7861f",
-  storageBucket: "vuesic-7861f.appspot.com",
-  messagingSenderId: "75078767129",
-  appId: "1:75078767129:web:a6db0613aa50151c989088"
-};
+  apiKey: 'AIzaSyBUoyQr4q_eXdpADcVrW7eCZRfppbZfNEg',
+  authDomain: 'vuesic-7861f.firebaseapp.com',
+  projectId: 'vuesic-7861f',
+  storageBucket: 'vuesic-7861f.appspot.com',
+  messagingSenderId: '75078767129',
+  appId: '1:75078767129:web:a6db0613aa50151c989088',
+}
